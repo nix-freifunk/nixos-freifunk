f7c0bcecadb814ded0a83483679c417b6780de12
Tom Herbers <mail@tomherbers.de>
2024-05-08 15:10:30 +0200

    meshviewer: allow enabling SSL

--- meshviewer/default.nix
+++ meshviewer/default.nix
@@ -4,7 +4,7 @@ with lib;
 let
   cfg = config.services.freifunk.meshviewer;
 
-  meshviewer = pkgs.callPackage ./pkg.nix {};
+  meshviewerPkg = pkgs.callPackage ./pkg.nix {};
   configFile = pkgs.writeText "config.json" (builtins.toJSON cfg.config);
 in
 
@@ -16,45 +16,53 @@ in
       default = false;
     };
 
+    enableSSL = mkOption {
+      type = types.bool;
+      default = false;
+      description = "Enable SSL for the meshviewer vhost";
+    };
+
+    useACMEHost = mkOption {
+      type = types.nullOr types.str;
+      default = null;
+      description = "The host to use for ACME";
+    };
+
     config = mkOption {
       type = types.attrs;
       default = {};
       description = "The meshviewer config.";
     };
 
-    nginx = {
-      enable = mkOption {
-        type = types.bool;
-        default = true;
-        description = "configure nginx virtual host";
-      };
-      hostName = mkOption {
-        type = types.str;
-        default = "meshviewer.example.org";
-        description = "domain to configure nginx for";
-      };
-      default = mkOption {
-        type = types.bool;
-        default = true;
-        description = "set as default nginx virtual host";
-      };
+    domain = mkOption {
+      type = types.str;
+      example = "meshviewer.example.org";
+      description = "The domain to use for the meshviewer vhost";
+    };
+
+    openFirewall = mkOption {
+      type = types.bool;
+      default = false;
+      description = "open firewall for nginx";
     };
   };
 
   config = mkIf cfg.enable {
 
-    services.nginx = mkIf cfg.nginx.enable {
-      enable = true;
-      virtualHosts."${cfg.nginx.hostName}" = {
-        default = cfg.nginx.default;
-        locations."/".root = "${meshviewer}";
+    services.nginx = {
+      enable = lib.mkDefault true;
+      virtualHosts."${cfg.domain}" = {
+        locations."/".root = "${meshviewerPkg}";
         locations."/".extraConfig = ''
           try_files $uri $uri/ =404;
         '';
         locations."=/config.json".alias = "${configFile}";
+
+        forceSSL = cfg.enableSSL;
+        useACMEHost = cfg.useACMEHost;
       };
     };
 
-    networking.firewall.allowedTCPPorts = [ 80 ];
+    networking.firewall.allowedTCPPorts = mkIf cfg.openFirewall [ 80 443 ];
   };
 }
