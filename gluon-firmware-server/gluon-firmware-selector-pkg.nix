8d7366a7e886f593e4d30f024a1d335073dddca6
Tom Herbers <mail@tomherbers.de>
2024-05-13 15:33:12 +0200

    gluon-firmware-selector: update from 0-unstable-2024-05-08 to 0-unstable-2024-05-13

--- gluon-firmware-server/gluon-firmware-selector-pkg.nix
+++ gluon-firmware-server/gluon-firmware-selector-pkg.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation rec {
   pname = "gluon-firmware-selector";
-  version = "0-unstable-2024-05-08";
+  version = "0-unstable-2024-05-13";
 
   src = fetchFromGitHub {
     owner = "freifunk-darmstadt";
     repo = "gluon-firmware-selector";
-    rev = "f8695356c1bca7e34b4a692e959fd0982978303a";
-    sha256 = "sha256-jzZLbue3h0wgzlFh10CpNYoWRXyv3WDdyuit9TzwNDw=";
+    rev = "eef379be93725491a32925d526e70d3364ad670a";
+    sha256 = "sha256-5xIV5sbDosOtK2tEUqq3dGX1MBrqawaZQfenyHVJq1A=";
   };
 
   sourceRoot = ".";
