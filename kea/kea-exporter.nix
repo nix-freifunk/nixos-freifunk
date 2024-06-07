bd85cea9da76bd699e50b8e51a69fb0a462b7b7c
Tom Herbers <mail@tomherbers.de>
2024-06-07 17:42:52 +0200

    kea-exporter: remove default firewall rules

--- kea/kea-exporter.nix
+++ kea/kea-exporter.nix
@@ -9,14 +9,4 @@
       "${config.services.kea.dhcp4.settings.control-socket.socket-name}"
     ];
   };
-
-  networking.nftables.tables.nixos-fw = {
-    content = ''
-      chain input_extra {
-        tcp dport ${toString config.services.prometheus.exporters.kea.port} ip saddr { 82.195.73.4, 10.223.254.14 } counter accept comment "prometheus-kea-exporter: accept from elsa"
-        tcp dport ${toString config.services.prometheus.exporters.kea.port} ip6 saddr { 2001:67c:2ed8::4:1, fd01:67c:2ed8:a::14:1 } counter accept comment "prometheus-kea-exporter: accept from elsa"
-      }
-    '';
-  };
-
 }
