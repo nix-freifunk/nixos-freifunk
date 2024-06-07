354d54c5b1ab90abec1c75937800fb445da27c60
Tom Herbers <mail@tomherbers.de>
2024-06-07 17:46:18 +0200

    kea-exporter: rename controlSocketPaths option to targets
    upstream change

--- kea/kea-exporter.nix
+++ kea/kea-exporter.nix
@@ -5,7 +5,7 @@
   services.prometheus.exporters.kea = {
     enable = true;
     listenAddress = "::";
-    controlSocketPaths = [
+    targets = [
       "${config.services.kea.dhcp4.settings.control-socket.socket-name}"
     ];
   };
