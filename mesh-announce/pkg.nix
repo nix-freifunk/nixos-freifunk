fb664dddc60afe2fa4610aeaab80be32d90e1d5b
Tom Herbers <mail@tomherbers.de>
2024-06-08 01:38:41 +0200

    mesh-announce: 0-unstable-2023-03-19 -> 0-unstable-2024-06-08
    ```
    4105913 remove enclosing quotes from lsb_release output
    ```

--- mesh-announce/pkg.nix
+++ mesh-announce/pkg.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation rec {
   pname = "mesh-announce";
-  version = "0-unstable-2023-03-19";
+  version = "0-unstable-2024-06-08";
 
   src = fetchFromGitHub {
     owner = "Freifunk-Rhein-Neckar";
     repo = pname;
-    rev = "4454451dde31bcfc3a8279d28fef4a33628d2c65";
-    sha256 = "sha256-znl9J667zVDjHOAH85HtrlL3lgmTpzkuLHHmEmqlF/w=";
+    rev = "4105913ce5a78972a92ffe35800f46639bf73771";
+    sha256 = "sha256-kPQErmAYNU6rHPgRmXYFFDUqRq4y21tPuPKVsl0TlWM=";
   };
 
   buildInputs = [ python3Packages.python ];
